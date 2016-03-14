3c5ea28fb3cc72bc0bb313d3cab23c663d776ffa
tdtd <tdtd.woodham@gmail.com>
2016-03-13 22:02:21 -0400

    1.0.0

--- server.js
+++ server.js
@@ -12,13 +12,8 @@ app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('/whoami', function(req,res) {
 	var whoami = {};
-	whoami.ipaddress =  req.headers['x-forwarded-for'] || req.connection.remoteAddress.match(/\:(?=[^:]*$)(.*)/)[1];
-	whoami.language = formatLanguage(req.headers["accept-language"]);
+	whoami.ipaddress = req.headers['x-forwarded-for'];
+	whoami.language = req.headers["accept-language"].split(',')[0];
 	whoami.software = req.headers["user-agent"].match(/\((.*?)\)/i)[1];
 	res.json(whoami);
 });
-
-function formatLanguage(lan){
-	var a = lan.split(',');
-	return a[0];
-}
